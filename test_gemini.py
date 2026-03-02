3b4eba6a6765afb8cecd0d5fa5ebd030881109ef
Manas Dhyani <98264626+manas-dhyani@users.noreply.github.com>
2026-03-02 09:30:49 +0530

    Delete test_gemini.py

--- test_gemini.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBCb1SUyeNoDMPWOpoSitdoaGOptqGvy-Q")
-
-model = genai.GenerativeModel("gemini-1.5-flash")
-
-response = model.generate_content("Write a poem about emotions.")
-
-print(response.text)
